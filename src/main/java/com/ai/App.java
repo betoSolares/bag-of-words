a0ad04fe1dbb1a1facd2b7a94e0228122f1ab7a8
Roberto Solares <betosolaresgar@gmail.com>
2021-05-02 19:37:55 -0600

    Create menu and parse options for train

--- src/main/java/com/ai/App.java
+++ src/main/java/com/ai/App.java
@@ -3,11 +3,51 @@ package com.ai;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.util.AbstractMap;
 
 public class App {
   public static void main(String[] args) {
+    int option = 0;
     parseFlags(args);
-    int option = mainMenu();
+
+    do {
+      option = mainMenu();
+
+      switch (option) {
+        case 1:
+          System.out.println();
+          int trainOption = TrainOption();
+
+          switch (trainOption) {
+            case 1:
+              AbstractMap.SimpleImmutableEntry<String, String> value = onePhrase();
+              if (value != null) {
+                // TODO: Train the model with a phrase
+                System.out.println(value);
+              }
+              break;
+
+            case 2:
+              String path = getPath();
+              if (path != null) {
+                // TODO: Train the model with a file
+                System.out.println(path);
+              }
+              break;
+          }
+
+          System.out.println();
+          break;
+
+        case 2:
+          System.out.println();
+          System.out.println("Infer");
+          System.out.println();
+          break;
+      }
+
+    } while (option != 3);
   }
 
   private static int mainMenu() {
@@ -28,8 +68,69 @@ public class App {
       }
     } while (option != 1 && option != 2 && option != 3);
 
-    System.out.println();
     return option;
+  }
+
+  private static int TrainOption() {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int option = 0;
+
+    do {
+      System.out.println("What kind of training do you want to do?");
+      System.out.println("  1) One phrase");
+      System.out.println("  2) Bulk file");
+      System.out.println("  3) None");
+      System.out.print("Put the number of the option: ");
+
+      try {
+        option = Integer.parseInt(br.readLine());
+      } catch (IOException | NumberFormatException e) {
+        System.out.println("That's not a valid option.\n");
+      }
+    } while (option != 1 && option != 2 && option != 3);
+
+    return option;
+  }
+
+  private static AbstractMap.SimpleImmutableEntry<String, String> onePhrase() {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String phrase = "";
+    String tag = "";
+
+    System.out.print("Insert the phrase: ");
+    try {
+      phrase = br.readLine();
+    } catch (IOException e) {
+      System.out.println("An error occurred reading the phrase");
+      return null;
+    }
+
+    System.out.print("Insert the tag: ");
+    try {
+      tag = br.readLine();
+    } catch (IOException e) {
+      System.out.println("An error occurred reading the tag");
+      return null;
+    }
+
+    return new AbstractMap.SimpleImmutableEntry<String, String>(phrase, tag);
+  }
+
+  private static String getPath() {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String path = "";
+
+    System.out.println("You are in: " + Path.of("").toAbsolutePath().toString());
+    System.out.print("Insert the path to the file: ");
+
+    try {
+      path = br.readLine();
+    } catch (IOException e) {
+      System.out.println("An error occurred reading the path");
+      return null;
+    }
+
+    return path;
   }
 
   private static void parseFlags(String[] flags) {
